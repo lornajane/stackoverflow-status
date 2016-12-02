ef01df16a72a6d6b9f8f962ee9e72a1d1a20e743
Raj Singh <rrsingh@us.ibm.com>
2016-12-02 15:10:53 -0500

    filters working, bad layout

---
+++ stackoverflow-status/public/js/filter.js
@@ -0,0 +1,48 @@
+var ACTIVE_TAG_CLASS = "blue";
+var INACTIVE_TAG_CLASS = "grey";
+var so_tags = [];
+
+$('.so-tag').each(function (stidx) {
+    so_tags.push($(this).text().trim());
+    updateCardVisibility();
+});
+
+function updateCardVisibility() {
+    $('.question').each(function (qidx) {
+        showme = false;
+        $(this).find('.tag').each(function (tidx) { // get question's tags
+            tagtext = $(this).text();
+            // check if any tag is in list of active tags
+            if (so_tags.indexOf(tagtext) >= 0) {
+                showme = true;
+                // if so, show it
+                return false;
+            }
+        });
+        if (showme) {
+            $(this).removeClass('notdisplayed');
+        } else {
+            $(this).addClass('notdisplayed');
+        }
+        // console.log( this.offsetHeight );
+    });
+}
+
+function setVisibleTags() {
+    so_tags = [];
+    $('.so-tag').each(function (stidx) {
+        if ( $(this).hasClass(ACTIVE_TAG_CLASS) ) {
+            so_tags.push($(this).text().trim());
+        }
+    });
+    updateCardVisibility();
+}
+
+function toggleTagVisibility(so_tag) {
+    if ( $(so_tag).hasClass(ACTIVE_TAG_CLASS) ) {
+        $(so_tag).removeClass(ACTIVE_TAG_CLASS).addClass(INACTIVE_TAG_CLASS);
+    } else {
+        $(so_tag).removeClass(INACTIVE_TAG_CLASS).addClass(ACTIVE_TAG_CLASS);
+    }
+    setVisibleTags();
+}
